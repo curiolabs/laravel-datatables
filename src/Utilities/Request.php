3415b2472ec1f1c8c2967438e378669b566ebfa8
curiolabs <36702970+curiolabs@users.noreply.github.com>
2018-10-12 17:14:20 +0100

    Update Request.php

--- src/Utilities/Request.php
+++ src/Utilities/Request.php
@@ -97,7 +97,11 @@ class Request
         $orderable = [];
         for ($i = 0, $c = count($this->request->input('order')); $i < $c; $i++) {
             $order_col = (int) $this->request->input("order.$i.column");
-            $order_dir = strtolower($this->request->input("order.$i.dir")) === 'asc' ? 'asc' : 'desc';
+            if ($this->request->input("order.$i.dir") == 'desc') {
+                $order_dir = 'desc';
+            } else {
+                $order_dir = 'asc';
+            }
             if ($this->isColumnOrderable($order_col)) {
                 $orderable[] = ['column' => $order_col, 'direction' => $order_dir];
             }
